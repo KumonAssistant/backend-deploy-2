98bd074401a7b2fed4f4626e772c714f53e18722
KumonAssistant <kumonassistant1@gmail.com>
2025-08-21 16:02:38 -0400

    Initial Setup

---
+++ index.js
@@ -0,0 +1,84 @@
+//const http = require('http') // Node's built-in web server module
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+// json-parser
+app.use(express.json())
+// CORS
+app.use(cors())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only javascript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+// createServer method of http module to create a new web server
+/*const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(notes))
+})*/
+
+app.get('/', (req, res) => {
+  res.send('<h1>hello world</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const note = notes.find(n => n.id === id)
+  
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(404).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    id: crypto.randomUUID(),
+    content: body.content,
+    important: body.important || false
+  }
+
+  notes = notes.concat(note)
+  res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  notes = notes.filter(n => n.id !== id)
+
+  req.status(204).end()
+})
+
+// listen to HTTP requests sent to port 3001
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
